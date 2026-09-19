b696a3cc5e57fc5a0cb5b17e7e47acad7ac6ab51
Greg Raven <greg_raven@hotmail.com>
2026-09-18 17:12:44 -0700

    update search code

--- header.php
+++ header.php
@@ -61,7 +61,7 @@ $logo_alt = function_exists( 'amren_field' ) ? amren_field( 'header_logo_alt', '
                     );
                     ?>
                     <form action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get" class="search-form" role="search">
-                        <input id="mobile-s" class="search-query" type="text" name="s" title="search" placeholder="Search...">
+                        <input id="s" class="search-query" type="text" name="s" title="search" placeholder="Search...">
                         <button type="submit" aria-label="Search">
                             <svg xmlns="http://www.w3.org/2000/svg" class="search-form-icon icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="#003c5e" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
                         </button>
